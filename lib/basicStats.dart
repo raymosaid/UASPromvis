bb899984fca2697de8bb0e113d478f1dba7baf23
jonaruo <jonathansuarapatty@gmail.com>
2023-01-06 11:52:08 +0700

    added Drawer to its own class
    drawer pindah ke kelasnya sendiri dan kode menjadi lebih rapih

--- lib/basicStats.dart
+++ lib/basicStats.dart
@@ -1,11 +1,7 @@
-import 'package:uaspromvisray/main.dart';
 import 'package:flutter/material.dart';
-import 'package:uaspromvisray/ability.dart';
-import 'package:uaspromvisray/equipment.dart';
-import 'package:uaspromvisray/dashboard.dart';
-import 'package:uaspromvisray/personality.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'Main_Model.dart';
+import 'drawer.dart';
 
 void main() {
   runApp(ScopedModel(
@@ -168,129 +164,7 @@ class BasicStatsPage extends StatelessWidget {
             ),
           ]),
 
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(''),
-                    maxRadius: 40,
-                  ),
-                  SizedBox(height: 10),
-                  Text(ScopedModel.of<MainModel>(context).data[kunci]['name'],
-                      style: TextStyle(fontSize: 15, color: Colors.white)
-                  ),
-                  Text(ScopedModel.of<MainModel>(context).data[kunci]['race'],
-                      style: TextStyle(fontSize: 10, color: Colors.white)
-                  ),
-                  Text(ScopedModel.of<MainModel>(context).data[kunci]['class'],
-                      style: TextStyle(fontSize: 10, color: Colors.white)
-                  )
-                ],
-              ),
-            ),
-            ListTile(
-              title: Text("Dashboard"),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Dashboard(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: Text("Ability Scores"),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => AbilityPage(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('Basic Stats'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => BasicStatsPage(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('Personality and Background'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PersonalityPage(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('Equipment'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => EquipmentPage(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-                title: Text("Attacks and Spellcasting"),
-                onTap: (){Navigator.of(context).pushNamed("/");}
-            ),
-            ListTile(
-                title: Text("Character Features"),
-                onTap: (){Navigator.of(context).pushNamed("/");}
-            ),
-            ListTile(
-                title: Text("Character Proficiency"),
-                onTap: (){Navigator.of(context).pushNamed("/");}
-            ),
-            ListTile(
-                title: Text("Inventory"),
-                onTap: (){Navigator.of(context).pushNamed("/");}
-            ),ListTile(
-                title: Text("Allies and Organizations"),
-                onTap: (){Navigator.of(context).pushNamed("/");}
-            ),
-            Container(
-                child: Align(
-                    alignment: FractionalOffset.bottomCenter,
-                    child: Column(
-                      children: <Widget>[
-                        Divider(),
-                        ListTile(
-                          leading: Icon(Icons.logout),
-                          title: Text('Log out'),
-                          onTap: (){
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => MyHomePage(),
-                              ),
-                            );
-                          },
-                        )
-                      ],
-                    )
-                )
-            ),
-          ],
-        ),
-      ),
+      drawer: myDrawer(),
     );
   }
 }
--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:uaspromvisray/ability.dart';
-import 'package:uaspromvisray/basicStats.dart';
-import 'package:uaspromvisray/equipment.dart';
-import 'package:uaspromvisray/main.dart';
-import 'package:uaspromvisray/personality.dart';
 import 'package:uaspromvisray/abilitychart.dart';
 import 'dart:io';
 import 'package:scoped_model/scoped_model.dart';
 import 'Main_Model.dart';
+import 'drawer.dart';
 
 void main() {
   runApp(MyApp());
@@ -40,129 +36,7 @@ class Dashboard extends StatelessWidget {
         appBar: AppBar(
           title: Text("Dashboard"),
         ),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                decoration: BoxDecoration(color: Colors.blue),
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(''),
-                      maxRadius: 40,
-                    ),
-                    SizedBox(height: 10),
-                    Text(ScopedModel.of<MainModel>(context).data[kunci]['name'],
-                        style: TextStyle(fontSize: 15, color: Colors.white)
-                    ),
-                    Text(ScopedModel.of<MainModel>(context).data[kunci]['race'],
-                        style: TextStyle(fontSize: 10, color: Colors.white)
-                    ),
-                    Text(ScopedModel.of<MainModel>(context).data[kunci]['class'],
-                        style: TextStyle(fontSize: 10, color: Colors.white)
-                    )
-                  ],
-                ),
-              ),
-              ListTile(
-                title: Text("Dashboard"),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Dashboard(),
-                    ),
-                  );
-                },
-              ),
-              ListTile(
-                title: Text("Ability Scores"),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AbilityPage(),
-                    ),
-                  );
-                },
-              ),
-              ListTile(
-                title: const Text('Basic Stats'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => BasicStatsPage(),
-                    ),
-                  );
-                },
-              ),
-              ListTile(
-                title: const Text('Personality and Background'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => PersonalityPage(),
-                    ),
-                  );
-                },
-              ),
-              ListTile(
-                title: const Text('Equipment'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => EquipmentPage(),
-                    ),
-                  );
-                },
-              ),
-              ListTile(
-                  title: Text("Attacks and Spellcasting"),
-                  onTap: (){Navigator.of(context).pushNamed("/");}
-              ),
-              ListTile(
-                  title: Text("Character Features"),
-                  onTap: (){Navigator.of(context).pushNamed("/");}
-              ),
-              ListTile(
-                  title: Text("Character Proficiency"),
-                  onTap: (){Navigator.of(context).pushNamed("/");}
-              ),
-              ListTile(
-                  title: Text("Inventory"),
-                  onTap: (){Navigator.of(context).pushNamed("/");}
-              ),ListTile(
-                  title: Text("Allies and Organizations"),
-                  onTap: (){Navigator.of(context).pushNamed("/");}
-              ),
-              Container(
-                  child: Align(
-                      alignment: FractionalOffset.bottomCenter,
-                      child: Column(
-                        children: <Widget>[
-                          Divider(),
-                          ListTile(
-                              leading: Icon(Icons.logout),
-                              title: Text('Log out'),
-                              onTap: (){
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => MyHomePage(),
-                                  ),
-                                );
-                              },
-                          )
-                        ],
-                      )
-                  )
-              ),
-            ],
-          ),
-        ),
+        drawer: myDrawer(),
         body: Center(
           child: ListView(
             children: [
