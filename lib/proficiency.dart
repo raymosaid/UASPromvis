74dadb53e38d3bf8d45b02c48beb1fb55d01a6f8
dheyahamida1510 <99865101+dheyahamida1510@users.noreply.github.com>
2023-01-06 12:17:50 +0700

    tambah 5 page
    halaman halaman kosong

---
+++ lib/proficiency.dart
@@ -0,0 +1,42 @@
+import 'package:uaspromvisray/drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'Main_Model.dart';
+
+void main() {
+  runApp(ScopedModel(
+    model: MainModel(),
+    child: MyApp(),
+  )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProficiencyPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class ProficiencyPage extends StatelessWidget {
+  TextEditingController AttackStats = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Attacks and Spellcastings"),
+      ),
+      drawer: myDrawer(),
+      body: Center(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.8,
+        ),
+      ),
+    );
+  }
+}
